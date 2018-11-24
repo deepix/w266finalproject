4ac5f29560225221e40ce3ffb87bff8ec13beedc
zach-merritt <zachmerritt@Zachs-MBP.home>
2018-11-24 13:34:07 -0500

    TPOT unigram implementation - still exploring

---
+++ feature_building/tpot_unigram.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+from sklearn.feature_selection import SelectPercentile, VarianceThreshold, f_classif
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+
+# NOTE: Make sure that the class is labeled 'target' in the data file
+tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
+features = tpot_data.drop('target', axis=1).values
+training_features, testing_features, training_target, testing_target = \
+            train_test_split(features, tpot_data['target'].values, random_state=42)
+
+# Score on the training set was:0.6506291953660375
+exported_pipeline = make_pipeline(
+    VarianceThreshold(threshold=0.45),
+    SelectPercentile(score_func=f_classif, percentile=32),
+    LogisticRegression(C=0.1, dual=True, penalty="l2")
+)
+
+exported_pipeline.fit(training_features, training_target)
+results = exported_pipeline.predict(testing_features)
